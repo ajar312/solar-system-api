a2071b87db3f887db97f98368f92da34ff7637e2
Ajar Duishembieva <ajduishe@gmail.com>
2024-10-26 00:44:31 -0700

    implement a route that creates a new data record using the Planet model; define routes that read model records; complete Wave 3

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,22 +1,44 @@
-from flask import Blueprint, abort, make_response
-#from .models import planets
+from flask import Blueprint, abort, make_response, request
+from app.models.planet import Planet
+from ..db import db
 
-planets_bp = Blueprint('planets', __name__, url_prefix="/planets")
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-# @planets_bp.get("")
-# def get_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append(
-#             {
-#                 "id": planet.id,
-#                 "name": planet.name,
-#                 "description": planet.description,
-#                 "color": planet.color,
-#             }
-#         )
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    color = request_body["color"]
 
-#     return planets_response
+    new_planet = Planet(name=name, description=description, color=color)
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "color": new_planet.color
+    }
+    return response, 201
+
+@planets_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    planets_response = []
+    for planet in planets:
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "color": planet.color,
+            }
+        )
+
+    return planets_response
 
 # @planets_bp.get("/<planet_id>")
 # def get_one_planet(planet_id):
