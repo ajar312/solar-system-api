5dd8b11ada2bbf53a61939dcb35aa45c36245d24
Dana Delgado <danamdelgadog@gmail.com>
2024-10-29 12:47:59 -0700

    Updated get_one_planet

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -44,7 +44,13 @@ def get_all_planets():
 def get_one_planet(planet_id):
     planet = validate_planet(planet_id)
     
-    return planet.to_dict()
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "color": planet.color,
+    }
+
 
 @planets_bp.put("/<planet_id>")
 def update_planet(planet_id):
